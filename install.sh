1e31520ad04ad8a7223951b8d53a18dc26c79a3c
1N3 <1N3@hushmail.com>
2015-09-06 11:45:45 -0400

    Sniper v1.3 by 1N3 @ CrowdShield

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Install script for sn1per
+#
+echo "Installing sn1per dependencies..."
+apt-get install host whois theharvester dnsenum curl nmap php5 php5-curl wapiti hydra iceweasel wpscan sqlmap arachni w3af golismero nbtscan enum4linux cisco-torch metasploit-framework theharvester nmap dnsenum snmpwalk nikto smtp-user-enum whatweb python nbtscan sslscan amap
+git clone https://github.com/1N3/Findsploit.git
+git clone https://github.com/1N3/BruteX.git
+git clone https://github.com/1N3/Goohak.git
+git clone https://github.com/1N3/XSSTracer.git
+git clone https://github.com/1N3/SuperMicro-Password-Scanner
+git clone https://github.com/Dionach/CMSmap.git
+mkdir loot
+echo "Be sure to install the following packages manually and update the sniper script references: dig dnsdict6 cmsmap samrdump inurlbr wafw00f showmount samrdump rpcinfo"
+echo "Done!"
