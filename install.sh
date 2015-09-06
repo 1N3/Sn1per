7acddbfa551c705f73d9a3df4042ced4386bae05
1N3 <1N3@hushmail.com>
2015-09-06 11:58:07 -0400

    Sniper v1.3 by 1N3 @ CrowdShield

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Install script for sn1per
 #
 echo "Installing sn1per dependencies..."
-apt-get install host whois theharvester dnsenum curl nmap php5 php5-curl wapiti hydra iceweasel wpscan sqlmap arachni w3af golismero nbtscan enum4linux cisco-torch metasploit-framework theharvester nmap dnsenum snmpwalk nikto smtp-user-enum whatweb python nbtscan sslscan amap
+apt-get install host whois theharvester dnsenum curl nmap php5 php5-curl wapiti hydra iceweasel wpscan sqlmap arachni w3af golismero nbtscan enum4linux cisco-torch metasploit-framework theharvester nmap dnsenum nikto smtp-user-enum whatweb python nbtscan sslscan amap
 git clone https://github.com/1N3/Findsploit.git
 git clone https://github.com/1N3/BruteX.git
 git clone https://github.com/1N3/Goohak.git
@@ -11,5 +11,5 @@ git clone https://github.com/1N3/SuperMicro-Password-Scanner
 git clone https://github.com/Dionach/CMSmap.git
 mkdir loot
 chmod +rx sniper
-echo "Be sure to install the following packages manually and update the sniper script references: dig dnsdict6 cmsmap samrdump inurlbr wafw00f showmount samrdump rpcinfo"
+echo "Be sure to install the following packages manually and update the sniper script references: dig dnsdict6 cmsmap samrdump inurlbr wafw00f showmount samrdump rpcinfo snmpwalk"
 echo "Done!"
