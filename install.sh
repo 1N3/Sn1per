85666329699d3034400271c41c9270b8c7230309
1N3 <1N3@hushmail.com>
2015-11-30 14:05:59 -0500

    Sn1per v1.4 by 1N3@CrowdShield

--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 # Install script for sn1per
 #
 echo "Installing sn1per dependencies..."
-apt-get install host whois theharvester dnsenum curl nmap php5 php5-curl wapiti hydra iceweasel wpscan sqlmap arachni w3af golismero nbtscan enum4linux cisco-torch metasploit-framework theharvester nmap dnsenum nikto smtp-user-enum whatweb python nbtscan sslscan amap
+apt-get install host whois theharvester dnsenum dnsrecon curl nmap php5 php5-curl wapiti hydra iceweasel wpscan sqlmap arachni w3af golismero nbtscan enum4linux cisco-torch metasploit-framework theharvester nmap dnsenum nikto smtp-user-enum whatweb python nbtscan sslscan amap
 git clone https://github.com/1N3/Findsploit.git
 git clone https://github.com/1N3/BruteX.git
 git clone https://github.com/1N3/Goohak.git
+cp Goohak/goohak /usr/bin/ -f
 git clone https://github.com/1N3/XSSTracer.git
+git clone https://github.com/1N3/MassBleed.git
 git clone https://github.com/1N3/SuperMicro-Password-Scanner
 git clone https://github.com/Dionach/CMSmap.git
+git clone https://github.com/0xsauby/yasuo.git
+git clone https://github.com/vishnuraju/Breach-Miner-automated-.git Breach-Miner
 mkdir loot
 chmod +rx sniper
 echo "Be sure to install the following packages manually and update the sniper script references: dig dnsdict6 cmsmap samrdump inurlbr wafw00f showmount samrdump rpcinfo snmpwalk"
