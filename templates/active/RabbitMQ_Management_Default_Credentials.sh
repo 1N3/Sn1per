0463dbf29e8e1ddc3e6e65801dfd0b330834253c
@xer0dayz <1n3@hushmail.com>
2020-09-14 08:54:19 -0700

    * v8.7 - Updated web file bruteforce lists * v8.7 - Added updated Slack API integration/notifications * v8.7 - Added Arachni, Nikto, Nessus, NMap + 20 passive sc0pe vulnerability parsers

--- templates/active/RabbitMQ_Management_Default_Credentials.sh
+++ templates/active/RabbitMQ_Management_Default_Credentials.sh
@@ -2,7 +2,7 @@ AUTHOR='@xer0dayz'
 VULN_NAME='RabbitMQ Management Default Credentials'
 URI="/api/whoami"
 METHOD='GET'
-MATCH="administrator"
+MATCH="{\"name\":\"guest\""
 SEVERITY='P2 - HIGH'
 CURL_OPTS='-H "Content-Type: application/json" -H "Authorization: Z3Vlc3Q6Z3Vlc3Q=" --user-agent '' -s -L --insecure'
 SECONDARY_COMMANDS=''
