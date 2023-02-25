bc26afa664d8fb74cc7c07939592447903e8d393
cgeopapa <cgeocodgod@gmail.com>
2023-02-25 13:06:42 +0200

    Added pagodo output redirect to file

--- modes/osint.sh
+++ modes/osint.sh
@@ -9,11 +9,11 @@ if [[ "$OSINT" = "1" ]]; then
 		echo -e "${OKGREEN}====================================================================================${RESET}•x${OKGREEN}[`date +"%Y-%m-%d](%H:%M)"`${RESET}x•"
 		cd $INSTALL_DIR/plugins/pagodo
 		if [[ "$PAGODO_PROXYCHAIN" == "1" ]]; then
-			proxychains4 python3 pagodo.py -d $TARGET -g ./dorks/all_google_dorks.txt -i 30 -x 120 -m 2 -s $LOOT_DIR/osint/dorks-$TARGET.txt > /dev/null &
+			proxychains4 python3 pagodo.py -d $TARGET -g ./dorks/all_google_dorks.txt -i 30 -x 120 -m 2 -s $LOOT_DIR/osint/dorks-$TARGET.txt &> $LOOT_DIR/osint/pagodo-logs-$TARGET.log &
 		elif [[ "$PAGODO_PROXYCHAIN" == "0" ]]; then
-			python3 pagodo.py -d $TARGET -g ./dorks/all_google_dorks.txt -i 30 -x 120 -m 2 -s $LOOT_DIR/osint/dorks-$TARGET.txt > /dev/null &
+			python3 pagodo.py -d $TARGET -g ./dorks/all_google_dorks.txt -i 30 -x 120 -m 2 -s $LOOT_DIR/osint/dorks-$TARGET.txt &> $LOOT_DIR/osint/pagodo-logs-$TARGET.log &
 		else
-			python3 pagodo.py -d $TARGET -g ./dorks/all_google_dorks.txt -i 30 -x 120 -m 2 -s $LOOT_DIR/osint/dorks-$TARGET.txt -p $PAGODO_PROXYCHAIN > /dev/null &
+			python3 pagodo.py -d $TARGET -g ./dorks/all_google_dorks.txt -i 30 -x 120 -m 2 -s $LOOT_DIR/osint/dorks-$TARGET.txt -p $PAGODO_PROXYCHAIN &> $LOOT_DIR/osint/pagodo-logs-$TARGET.log &
 		fi
 	fi
 	if [[ "$WHOIS" == "1" ]]; then
